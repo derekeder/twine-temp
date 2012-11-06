f2bc2f5ed0a8fc21e1c6b6277cfe292d1b9542a3
Derek Eder <derek.eder@gmail.com>
2012-11-06 15:56:55 -0600

    home page displaying recorded temperatures

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @temperature_readings = TemperatureReading.all
+    respond_to do |format|
+      format.html  # index.html.erb
+      format.json  { render :json => @posts }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 TwineTemp::Application.routes.draw do
 
-  get 'temperature_reading/' => 'temperature_reading#record_temp'
+  get 'temperature_reading/' => 'temperature_reading#index'
+  get 'temperature_reading/save' => 'temperature_reading#record_temp'
 
   root :to => 'home#index'
 end

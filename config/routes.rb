7ebbb7dca0cf8211c1db60dc4dd98bd36acbfb0d
Derek Eder <derek.eder@gmail.com>
2013-01-09 09:40:05 -0600

    updated to Rails 3.2.11, removed unused route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 TwineTemp::Application.routes.draw do
 
-  get 'temperature_reading/' => 'temperature_reading#index'
   get 'temperature_reading/save' => 'temperature_reading#record_temp'
 
   root :to => 'home#index'
